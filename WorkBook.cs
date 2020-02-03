c6dd2482295bce14f5d64645dad958027b113b6d
FaustVX <jodu035@gmail.com>
2020-02-03 18:38:36 +0100

    Added WorkSheet

--- WorkBook.cs
+++ WorkBook.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
 
 namespace Excel.NET
 {
@@ -6,9 +7,20 @@ namespace Excel.NET
     {
         private readonly Workbook _workbook;
 
+        private readonly Dictionary<string, WorkSheet> _worksheets = new Dictionary<string, WorkSheet>();
+
         public WorkBook(Workbook workbook)
         {
             _workbook = workbook;
         }
+
+        public WorkSheet ActiveSheet
+            => GetSheet((Worksheet)_workbook.ActiveSheet);
+
+        public WorkSheet Sheet(string name)
+            => GetSheet((Worksheet)_workbook.Worksheets[name]);
+
+        private WorkSheet GetSheet(Worksheet sheet)
+            => _worksheets.TryGetValue(sheet.Name, out var s) ? s : _worksheets[sheet.Name] = new WorkSheet(sheet);
     }
 }
---
+++ WorkSheet.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Excel.NET
+{
+    public class WorkSheet
+    {
+        private readonly Worksheet _worksheet;
+
+        public WorkSheet(Worksheet worksheet)
+        {
+            _worksheet = worksheet;
+        }
+
+        public Range Range(string start)
+            => _worksheet.Range[start];
+
+        public Range Range(string start, string end)
+            => _worksheet.Range[start, end];
+    }
+}

b023d2f9e46ef05cb9356edabf826cb060f4711c
FaustVX <jodu035@gmail.com>
2020-02-03 18:07:02 +0100

    Initial commit

---
+++ Application.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using EX = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+
+namespace Excel.NET
+{
+    public static class Application
+    {
+        private static readonly EX.Application _app = new EX.Application();
+
+        private static readonly Dictionary<string, WorkBook> _workbooks = new Dictionary<string, WorkBook>();
+
+        public static WorkBook OpenWorkBook(string fileName)
+        {
+            var path = new FileInfo(fileName);
+            if (_workbooks.TryGetValue(path.Name, out var wb))
+                return wb;
+            return _workbooks[path.Name] = new WorkBook(_app.Workbooks.Open(path.FullName));
+        }
+
+        public static WorkBook GetWorkBook(string name)
+            => _workbooks[name];
+    }
+}
---
+++ WorkBook.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Excel.NET
+{
+    public class WorkBook
+    {
+        private readonly Workbook _workbook;
+
+        public WorkBook(Workbook workbook)
+        {
+            _workbook = workbook;
+        }
+    }
+}
